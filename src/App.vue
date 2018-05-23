98076e4f5be956ab6ce4b21a8c5c35206b4dc846
Austin Stevens <austinestevens@outlook.com>
2018-05-23 14:52:04 -0400

    Added Vue boilerplate from CLI and removed placeholder text/image.

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+	<h1>Nimbus</h1>
+	<h5>Network Activity Monitor</h5>
+	
+	<svg id="network-data"></svg>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  data: () => {
+    return {
+      seen: true
+	}
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #415059;
+  font-family: Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #fff;
+}
+
+#app {
+  
+}
+</style>
